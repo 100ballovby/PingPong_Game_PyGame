3d6d747db51b4a9f7ced0bae4005c94e85ee5161
GreatRaksin <greatraksin@icloud.com>
2024-05-04 15:19:24 +0300

    Adding timer and restart

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import sys
 import pygame as pg
+from random import choice
+
+
+def restart(obj, s_width, s_height, s):
+    global speed_x, speed_y, score_time
+    obj.center = (s_width // 2, s_height // 2)
+    speed_x *= choice([-1, 1])
+    speed_y *= choice([-1, 1])
+
+    cur_time = pg.time.get_ticks()
+    if cur_time - score_time < 700:
+        num_3 = score_font.render('3', True, BLUE)
+        screen.blit(num_3, [s_width // 2, s_height // 2])
+    elif cur_time - score_time < 1400:
+        num_2 = score_font.render('2', True, BLUE)
+        screen.blit(num_2, [s_width // 2, s_height // 2])
+    elif cur_time - score_time < 2100:
+        num_1 = score_font.render('1', True, BLUE)
+        screen.blit(num_1, [s_width // 2, s_height // 2])
+
+    if cur_time - score_time < 2100:
+        speed_x, speed_y = 0, 0
+    else:
+        speed_x = s * choice([-1, 1])
+        speed_y = s * choice([-1, 1])
+        score_time = None  # выключаем триггер
 
 
 def ball_move(obj, s_with, s_height, pl, opp):
-    global speed_x, speed_y  # объявляю переменные speed_x и speed_y из глобального пространства имен ГЛОБАЛЬНЫМИ для функции в тч
+    global speed_x, speed_y, p_score, o_score, score_time  # объявляю переменные speed_x и speed_y из глобального пространства имен ГЛОБАЛЬНЫМИ для функции в тч
     obj.x += speed_x
     obj.y += speed_y
 
     if obj.top <= 0 or obj.bottom >= s_height:
         speed_y *= -1
-    elif obj.left <= 0 or obj.right >= s_with:
-        speed_x *= -1
+    elif obj.left <= 0:
+        score_time = pg.time.get_ticks()  # фиксация "времени", когда был забит гол
+        p_score += 1
+    elif obj.right >= s_with:
+        score_time = pg.time.get_ticks()  # фиксация "времени", когда был забит гол
+        o_score += 1
     elif obj.colliderect(pl) or obj.colliderect(opp):
         speed_x *= -1
 
@@ -57,9 +87,15 @@ ball.center = (W // 2, H // 2)
 # game config
 speed = 7
 p_speed = 0
-o_speed = speed * 0.7
+o_speed = speed * 0.8
 ball_moving = False
-speed_x = speed_y = speed
+speed_x = speed * choice([-1, 1])
+speed_y = speed * choice([-1, 1])
+score_time = True
+
+p_score, o_score = 0, 0
+pg.font.init()
+score_font = pg.font.SysFont('comicsans', 64)
 
 pg.init()  # инициализируем pygame
 screen = pg.display.set_mode((W, H))  # создаем экран игры разрешением 1280х720px
@@ -77,6 +113,14 @@ while True:  # цикл игры
     pg.draw.rect(screen, BLUE, opponent)
     pg.draw.aaline(screen, WHITE, [W // 2, 0], [W // 2, H])
     pg.draw.ellipse(screen, BLUE, ball)
+
+    p_score_text = score_font.render(str(p_score), True, BLUE)
+    o_score_text = score_font.render(str(o_score), True, BLUE)
+    screen.blit(p_score_text, [W // 2 + 50, H * 0.25])
+    screen.blit(o_score_text, [W // 2 - 85, H * 0.25])
+
+    if score_time:
+        restart(ball, W, H, speed)
 
     pg.display.update()
 
